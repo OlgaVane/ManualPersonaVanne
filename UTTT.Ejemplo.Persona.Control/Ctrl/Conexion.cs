ffe6cab5421211d03ab2bc8463a564fbf1879540
OlgaVane <18300199@uttt.edu.mx>
2021-01-13 11:09:47 -0600

    version 1

---
+++ UTTT.Ejemplo.Persona.Control/Ctrl/Conexion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace UTTT.Ejemplo.Persona.Control.Ctrl
+{
+    public class Conexion
+    {
+        #region Variables
+     
+        #endregion
+
+        #region Constructores
+        public Conexion()
+        {
+        }       
+        #endregion
+
+
+        public SqlConnection sqlConnection()
+        {
+            try
+            {
+                SqlConnection conexion = new SqlConnection("Data Source=DESKTOP-G44JET2\\MSSQLSERVER;Initial Catalog=Manual;User ID=sa;Password=1234");
+                return conexion;
+            }
+            catch (Exception _e)
+            { 
+            
+            }
+            return null;
+        }
+    }
+}
